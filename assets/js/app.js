1136ec457a9203cc7b6a63812dbd8247249db60e
NatyNatur <naticcg@gmail.com>
2018-02-07 18:04:45 -0300

    completando 'trazarRuta', agregando eslint

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,17 @@ var latitude, longitude;
 document.getElementById('findMe').addEventListener('click', search);
 
 function initMap() {
-  var location = {lat: -33.45, lng: -70.6667};
+  var location = {
+    lat: -33.45,
+    lng: -70.6667
+  };
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 15,
-  center: location
+    zoom: 15,
+    center: location
   });
   var marker = new google.maps.Marker({
-  position: location,
-  map: map
+    position: location,
+    map: map
   });
 
   var inputPartida = document.getElementById('startingPoint');
@@ -18,35 +21,59 @@ function initMap() {
 
   new google.maps.places.Autocomplete(inputPartida);
   new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay) {
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status) {
+      if (status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        window.alert('No encontramos una ruta');
+      }
+    });
+  };
+
+  // mapa mostrará la ruta
+  directionsDisplay.setMap(map);
+  var trazarRuta = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('goTo').addEventListener('click', trazarRuta);
 };
 
-
-function search () {
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error)
+function search() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
   }
 }
 
-var success = function getLocationSuccess (position) {
+var success = function getLocationSuccess(position) {
   latitude = position.coords.latitude;
   longitude = position.coords.longitude;
-  var location = {lat: latitude, lng: longitude};
+  var location = {
+    lat: latitude,
+    lng: longitude
+  };
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 17,
-  center: location
+    zoom: 17,
+    center: location
   });
   var marker = new google.maps.Marker({
-  position: location,
-  map: map
+    position: location,
+    map: map
   });
-  /*map.setZoom(16);
+  /* map.setZoom(16);
   map.setCenter(position);*/
+};
 
-
-}
-
-var error = function (error) {
+var error = function(error) {
   alert('Tenemos problemas para encontrar tu ubicación.');
-}
+};
 
 
