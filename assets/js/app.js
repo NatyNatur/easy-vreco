3d9bd88e7c181773448af0f3ed425dc67a62c7cf
tamybl <tamara.bravo.l@gmail.com>
2018-02-07 15:42:30 -0300

    Subiendo 1 de ejercicio Encuentrame

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,44 @@
-var success = function getLocationSucces (position) {
-  latitude = position.coords.latitude;
-  longitude = position.coords.longitude;
-  navigator.geolocation
-}
+var latitude, longitude;
 
-var error = function (error) {
-  alert('Tenemos problemas para encontrar tu ubicación.');
-}
+document.getElementById('encuentrame').addEventListener('click', search);
 
 function initMap() {
   var location = {lat: -25.363, lng: 131.044};
   var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 12,
+  zoom: 15,
   center: location
   });
   var marker = new google.maps.Marker({
   position: location,
   map: map
   });
+}  
+
+function search () {
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error)
+  }
 }
+
+var success = function getLocationSuccess (position) {
+  latitude = position.coords.latitude;
+  longitude = position.coords.longitude;
+  var location = {lat: latitude, lng: longitude};
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 15,
+  center: location
+  });
+  var marker = new google.maps.Marker({
+  position: location,
+  map: map
+  });
+  map.setZoom(18);
+  map.setCenter(position);
+}
+
+var error = function (error) {
+  alert('Tenemos problemas para encontrar tu ubicación.');
+}
+
+
+
